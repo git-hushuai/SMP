22b2bf8dd537f945e81511980e32bbc0038c8ff3
shuaike <718401263@qq.com>
2019-07-12 17:27:43 +0800

    no message

--- app.js
+++ app.js
@@ -6,34 +6,69 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
+    this.checkUserAuthorization();
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+    // wx.login({
+    //   success: res => {
+    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    //   }
+    // })
     // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+    // wx.getSetting({
+    //   success: res => {
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //       wx.getUserInfo({
+    //         success: res => {
+    //           // 可以将 res 发送给后台解码出 unionId
+    //           this.globalData.userInfo = res.userInfo
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //           // 所以此处加入 callback 以防止这种情况
+    //           if (this.userInfoReadyCallback) {
+    //             this.userInfoReadyCallback(res)
+    //           }
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
+  },
+// 检查用户是否授权
+  checkUserAuthorization(url){
+
+    let that = this;
+    this.globalData.unionId = wx.getStorageSync("unionId") || '';
+    this.globalData.accessToken = wx.getStorageSync('accessToken') || '';
+
+    wx.getSetting({
+      success(res) {
+        console.log("authSetting" + res.authSetting);
+        debugger
+        if(res.authSetting["scope.userinfo"]){
+            wx.getUserInfo({
+              success : function(res){
+                 console.log('用户已经授权');
               }
-            }
+            })
+        }else{
+          // 未授权,去到授权界面
+          wx.redirectTo({
+            url: '/pages/authorization/index',
           })
+
         }
       }
     })
+
   },
+
+
+
   globalData: {
-    userInfo: null
+    userInfo: null,
+    unionId : null,
+    accessToken : null
+
   }
 })
---
+++ pages/authorization/index.js
@@ -0,0 +1,81 @@
+
+const app = getApp()
+
+Page({
+
+callbackUrl : "",
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.callbackUrl = decodeURIComponent(options.callbackUrl) || "/pages/index/index";
+  },
+
+  getUserInfo(e) {
+    if (e.detail.errMsg == 'getUserInfo:fail auth deny') {
+      return false;
+    }
+    debugger
+    // 已经授权，可以去后台获取回话token
+    wx.switchTab({
+      url: '/pages/index/index',
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
